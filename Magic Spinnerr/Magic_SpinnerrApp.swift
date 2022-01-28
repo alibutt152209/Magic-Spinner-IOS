312a5f7383356240f43ce9957a17962d4806203b
Ali Butt <alibutt152209@gmail.com>
2022-01-28 15:27:14 +0500

    Initial Commit

---
+++ Magic Spinnerr/Magic_SpinnerrApp.swift
@@ -0,0 +1,17 @@
+//
+//  Magic_SpinnerrApp.swift
+//  Magic Spinnerr
+//
+//  Created by Ali Butt on 28/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Magic_SpinnerrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
